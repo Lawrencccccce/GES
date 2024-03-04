6cdf0e37364a9e42e49a20bfd3eafa03c204d774
Lawrence <xiaoxuan.li.w@gmail.com>
2024-03-04 19:01:00 +1100

    add some plot

--- demo.py
+++ demo.py
@@ -1,9 +1,28 @@
 from ges.main import fit_bic
+from datetime import datetime
+from pytz import timezone
 import io
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+import pathlib
+import sys
 
+
+
+def create_dir(dir_path):
+    """
+    dir_path - A path of directory to create if it is not found
+    :param dir:
+    :return exit_code: 0:success -1:failed
+    """
+    try:
+        if not os.path.exists(dir_path):
+            pathlib.Path(dir_path).mkdir(parents=True, exist_ok=True)
+
+        return 0
+    except Exception as err:
+        sys.exit(-1)
 
 
 
@@ -14,13 +33,31 @@ if __name__ == "__main__":
     datapath = os.path.join('datasets', 'LUCAS.npy')
     datapath = os.path.join(current_dir, datapath)
 
+    sol_path = os.path.join('datasets', 'LUCAS_sol.npy')
+    sol_path = os.path.join(current_dir, sol_path)
+
+    plot_dir = os.path.join(current_dir, 'plots')
+    create_dir(plot_dir)
+
     data = np.load(datapath).astype(np.float32)
+    ground_truth = np.load(sol_path).astype(np.float32)
     print(data)
+    print(ground_truth)
 
 
     estimate_g, score = fit_bic(data)
     print(estimate_g)
     print(score)
+
+    fig = plt.figure(3)
+    ax = fig.add_subplot(1, 2, 1)
+    ax.set_title('est_graph')
+    ax.imshow(np.around(estimate_g).astype(int),cmap=plt.cm.binary)
+    ax = fig.add_subplot(1, 2, 2)
+    ax.set_title('true_graph')
+    ax.imshow(ground_truth, cmap=plt.cm.binary)
+    plt.savefig('{}/estimated_graph_{}.png'.format(plot_dir, datetime.now(timezone('Australia/Sydney')).strftime('%Y-%m-%d_%H-%M-%S-%f')[:-3]))
+    plt.close()
 
 
 

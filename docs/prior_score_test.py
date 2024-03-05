57361b7e9dde14f2da153ba052c7223d1c6859d5
Lawrence <xiaoxuan.li.w@gmail.com>
2024-03-05 11:52:31 +1100

    add prior score class

---
+++ docs/prior_score_test.py
@@ -0,0 +1,25 @@
+import numpy as np
+import sempler
+import ges
+import ges.scores
+
+
+
+
+
+if __name__ == "__main__":
+    # Generate observational data from a Gaussian SCM using sempler
+    A = np.array([[0, 0, 1, 0, 0],
+                [0, 0, 1, 0, 0],
+                [0, 0, 0, 1, 1],
+                [0, 0, 0, 0, 1],
+                [0, 0, 0, 0, 0]])
+    W = A * np.random.uniform(1, 2, A.shape) # sample weights
+    data = sempler.LGANM(W,(1,2), (1,2)).sample(n=5000)
+
+    score_class = ges.scores.PriorScore(data)
+
+    # Run GES with the gaussian BIC score
+    estimate, score = ges.fit(score_class)
+
+    print(estimate, score)
---
+++ ges/scores/prior_socres.py
@@ -0,0 +1,25 @@
+
+
+
+from .gauss_obs_l0_pen import GaussObsL0Pen
+from .prior_knowledge import PriorKnowledge
+
+
+
+
+
+
+
+
+
+class PriorScore(GaussObsL0Pen):
+
+
+    def __init__(self, data, lmbda=None, method='scatter', cache=True, debug=0):
+        super().__init__(data = data, lmbda = lmbda, method = method, cache = cache, debug = debug)
+        self.prior = PriorKnowledge()
+
+
+
+
+
